ccccca3d0be4ab99fc7b1436f8b2f77ebca1e3bd
Christopher Medina Velarde <ch.rash37@gmail.com>
2021-05-07 01:38:20 -0400

    varios cambios: Agregar pasantia vacia, Formulario persistente, Campos de carrera y url imagen formato password, campo de requisitos con otro formato, campo de correo con formato de correo, boton solo permite enviar cuando todo se llena, validaciones, indica el error cuando algo falla en un campo

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,28 +1,74 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnDestroy, OnInit {
 
-  loginForm = new FormGroup({
-    nombrePasantia: new FormControl(''),
-    empresa: new FormControl(''),
-    requisitos: new FormControl(''),
-    correoDeContacto: new FormControl(''),
-    carrera: new FormControl('')
+   emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+  createFormGroup(){
+  return new FormGroup({
+    nombre: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]),
+    empresa: new FormControl('',[Validators.required, Validators.minLength(5), Validators.maxLength(20)]),
+    requisistos: new FormControl('',[Validators.required, Validators.minLength(5), Validators.maxLength(20)]),
+    correo: new FormControl('',[Validators.required, Validators.pattern(this.emailPattern)]),
+    area: new FormControl('',[Validators.required, Validators.minLength(5), Validators.maxLength(20)]),
+    logo:new FormControl('',[Validators.required, Validators.minLength(5), Validators.maxLength(20)]),
     });
+  }
+    loginForm: FormGroup;
 
-  constructor() {}
+    private path='pasantias/'
+  constructor(private router: Router,private pasantiaServeice:PasantiaService ) {
+    this.loginForm=this.createFormGroup();
+  }
+
+  ngOnInit():void {
+    this.onResetForm();
+  }
+  onResetForm(){
+    this.loginForm.reset();
+  }
+
+  onSaveForm(){
+
+  }
+
+  get nombre() {return this.loginForm.get('nombre');}
+  get empresa() {return this.loginForm.get('empresa');}
+  get requisistos() {return this.loginForm.get('requisistos');}
+  get correo() {return this.loginForm.get('correo');}
+  get area() {return this.loginForm.get('area');}
+  get logo() {return this.loginForm.get('logo');}
+
+  ionViewDidLeave(){
+    this.loginForm.reset();
+  }
+
+  ngOnDestroy() {
+
+  }
 
   pasantia(){
 
   }
 
   input(){
+
+  }
+  guardarPasantia(){
+
+    const id= this.pasantiaServeice.getID();
+    this.pasantiaServeice.createPasantias(this.loginForm.value,this.path,id);
+    this.onResetForm();
+    this.router.navigate(['/tabs/tab1'])
+    console.log('form',this.loginForm.value)
 
   }
 
